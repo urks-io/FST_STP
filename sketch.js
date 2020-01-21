6a5ed2c5041d923062b2f728daa1dd6b43b40054
urkskur <urkskurth@gmail.com>
2020-01-21 10:37:39 -0800

    first commit

--- sketch.js
+++ sketch.js
@@ -1,7 +1,37 @@
+
+
+
 function setup() {
-  // put setup code here
+  createCanvas(400, 600);
+
 }
 
 function draw() {
-  // put drawing code here
+   background(15, 10, 95);
+   noStroke();
+  fill(255, 204, 0, 99);
+  circle(233, 153, 250);
+ 
+ 
+  translate(200, 220);
+ 
+
+  fill(255, 204, 0);
+ beginShape();
+ vertex(40,20);
+  vertex(40,120);
+  vertex(60, 100);
+  vertex(40, 220);
+  vertex(60, 200);
+  vertex(40, 340);
+  vertex(120, 150);
+  vertex(100, 170);
+  vertex(120, 70);
+  vertex(100, 90);
+  vertex(120, 20);
+  endShape(CLOSE);
+
+
+  createColorPicker();
+
 }
